e229616e8332e04b592e49379579c99288c8dcca
radoikoff <radoikoff@gmail.com>
2017-01-21 18:35:53 +0200

    Added three new projects

---
+++ SquareFrame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareFrame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string line = "+ ";
+            for (int i = 1; i <= n-2; i++)
+            {
+                line += "- ";
+            }
+            line += "+";
+            //Console.WriteLine(line);
+            string line2 = "| ";
+            for (int i = 1; i <= n - 2; i++)
+            {
+                line2 += "- ";
+            }
+            line2 += "|";
+            //Console.WriteLine(line);
+
+
+                Console.WriteLine(line);
+                for (int j = 0; j < n-2; j++)
+                {
+                    Console.WriteLine(line2);
+                }
+                Console.WriteLine(line);
+
+
+        }
+    }
+}
